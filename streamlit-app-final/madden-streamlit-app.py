fe0c6ed416d7c74f30d13cfb5e1e86f295b0bb04
andrewoels <andrew.oelschlager@gmail.com>
2025-04-27 21:11:00 -0400

    Update madden-streamlit-app.py

--- streamlit-app-final/madden-streamlit-app.py
+++ streamlit-app-final/madden-streamlit-app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/Madden 25 Ratings.csv")
+    df = pd.read_csv("data/Madden25-Ratings.csv")
 
     # Step 1: Combine first and last names
     df["Name"] = df["firstName"] + " " + df["lastName"]
